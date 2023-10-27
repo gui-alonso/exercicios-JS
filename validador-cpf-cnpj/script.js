eec07a05a1977e1a195b855087eb1a1dddd2f555
Guilherme <alonsogui011@gmail.com>
2023-10-27 09:20:47 -0300

    new updates

--- validador-cpf-cnpj/script.js
+++ validador-cpf-cnpj/script.js
@@ -1,44 +1,28 @@
 const tipoDocumento = document.getElementById("tipoDocumento");
 const documentoInput = document.getElementById("documentoInput");
+const isValid = validarDocumento(tipoDocumento);
 
-// Função para alternar entre CPF e CNPJ
-tipoDocumento.addEventListener("change", function() {
-    if (tipoDocumento.value === "cpf") {
-        documentoInput.placeholder = "Digite o CPF";
-        applyCPFMask();
-    } else if (tipoDocumento.value === "cnpj") {
-        documentoInput.placeholder = "Digite o CNPJ";
-        applyCNPJMask();
+// Função para aplicar a máscara de acordo com o tipo de documento
+function applyMask() {
+let value = documentoInput.value.replace(/\D/g, "");
+if (tipoDocumento.value === "cpf") {
+    if (value.length > 11) {
+        value = value.slice(0, 11);
     }
-});
-
-// Função para aplicar a máscara de CPF
-function applyCPFMask() {
-    documentoInput.removeEventListener("input", applyCNPJMask);
-    documentoInput.addEventListener("input", function() {
-        let value = documentoInput.value.replace(/\D/g, "");
-        if (value.length > 11) {
-            value = value.slice(0, 11);
-        }
-        value = value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
-        documentoInput.value = value;
-    });
+    value = value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+} else if (tipoDocumento.value === "cnpj") {
+    if (value.length > 14) {
+        value = value.slice(0, 14);
+    }
+    value = value.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5");
+}
+documentoInput.value = value;
 }
 
-// Função para aplicar a máscara de CNPJ
-function applyCNPJMask() {
-    documentoInput.removeEventListener("input", applyCPFMask);
-    documentoInput.addEventListener("input", function() {
-        let value = documentoInput.value.replace(/\D/g, "");
-        if (value.length > 14) {
-            value = value.slice(0, 14);
-        }
-        value = value.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5");
-        documentoInput.value = value;
-    });
-}
+// Função para limpar o campo de CPF
+function limparCampo() {
+    document.getElementById("cpfInput").value = ""; // Define o valor do campo como vazio
+  }
 
-// Inicialmente, aplicar a máscara de CPF
-applyCPFMask();
-// Incicialmente, aplicar a máscara de CNPJ
-applyCNPJMask();
+// Adiciona o evento de input para aplicar a máscara
+documentoInput.addEventListener("input", applyMask);
